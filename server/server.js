06d6f550c7029a2c4ccdb1c7d68a05f463d9d704
amir-mostafa-hs <amir-mostafa-hs@outlook.com>
2024-07-05 22:09:57 +0330

    step two in backend

--- server/server.js
+++ server/server.js
@@ -1,11 +1,47 @@
 const express = require("express");
+const dotenv = require("dotenv");
+dotenv.config();
+const mongoose = require("mongoose");
+const TasksModel = require("./models/tasks.model");
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(5000, () => {
-  console.log("Server is running on port:", 5000);
+app.get("/tasks", async (req, res) => {
+  try {
+    const result = await TasksModel.find({});
+    res.json(result);
+  } catch (error) {
+    res.json({ errorMessage: error });
+  }
 });
+
+app.post("/tasks", async (req, res) => {
+  try {
+    const data = req.body;
+    const result = await TasksModel.create({ ...data });
+    res.json(result);
+  } catch (error) {
+    res.json({ errorMessage: error });
+  }
+});
+
+async function connectDB(uri) {
+  try {
+    await mongoose.connect(uri, {
+      dbName: "Todo-list",
+    });
+
+    app.listen(5000, () => {
+      console.log("Server is running on port:", 5000);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+}
+connectDB(process.env.MONGODB_URI);
